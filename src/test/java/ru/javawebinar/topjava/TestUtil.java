3407492e887f6fc818dec00a3075b0049c4ec6e8
vitov <vitovt.kublitsky@mail.ru>
2021-10-27 16:43:55 +0300

    HW7 1.1 complete RootControllerTest

--- src/test/java/ru/javawebinar/topjava/TestUtil.java
+++ src/test/java/ru/javawebinar/topjava/TestUtil.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava;
 
+import org.assertj.core.matcher.AssertionMatcher;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import ru.javawebinar.topjava.web.json.JsonUtil;
@@ -22,5 +23,28 @@ public class TestUtil {
 
     public static <T> List<T> readListFromJsonMvcResult(MvcResult result, Class<T> clazz) throws UnsupportedEncodingException {
         return JsonUtil.readValues(getContent(result), clazz);
+    }
+
+    public static <T> AssertionMatcher<T> getMatcher(TestMatcher<T> matcher, T expected) {
+        return new AssertionMatcher<>() {
+            @Override
+            public void assertion(T actual) throws AssertionError {
+                matcher.assertMatch(actual, expected);
+            }
+        };
+    }
+
+    @SafeVarargs
+    public static <T> AssertionMatcher<Iterable<T>> getMatcher(TestMatcher<T> matcher, T... expected) {
+        return getMatcher(matcher, List.of(expected));
+    }
+
+    public static <T> AssertionMatcher<Iterable<T>> getMatcher(TestMatcher<T> matcher, Iterable<T> expected) {
+        return new AssertionMatcher<>() {
+            @Override
+            public void assertion(Iterable<T> actual) throws AssertionError {
+                matcher.assertMatch(actual, expected);
+            }
+        };
     }
 }
