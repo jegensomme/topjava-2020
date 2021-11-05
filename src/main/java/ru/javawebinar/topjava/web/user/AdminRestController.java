48840ca00ab2966b4c36db27e08235bf5120e7c8
vitov <vitovt.kublitsky@mail.ru>
2021-11-05 11:42:40 +0300

    9 04 HW8 with meals

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -56,4 +56,16 @@ public class AdminRestController extends AbstractUserController {
     public User getByMail(@RequestParam String email) {
         return super.getByMail(email);
     }
+
+    @Override
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable int id, @RequestParam boolean enabled) {
+        super.enable(id, enabled);
+    }
+
+    @GetMapping("/{id}/with-meals")
+    public User getWithMeals(@PathVariable int id) {
+        return super.getWithMeals(id);
+    }
 }
